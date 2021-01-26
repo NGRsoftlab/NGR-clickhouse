675c253b0780fa08b2c55a5365207813deae7db8
m.gavrilenko <m.gavrilenko@ngrsoftlab.ru>
2021-01-26 12:42:24 +0300

    Test for Parse() fix

--- dataparser_test.go
+++ dataparser_test.go
@@ -370,3 +370,305 @@ func TestParseData(t *testing.T) {
 	}
 }
 
+func TestParseDataNewNullableArray(t *testing.T) {
+	type testCase struct {
+		name          string
+		inputtype     string
+		inputopt      *DataParserOptions
+		inputdata     string
+		output        interface{}
+		failParseDesc bool
+		failNewParser bool
+		failParseData bool
+	}
+
+	losAngeles, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		t.Fatalf("failed to load time zone America/Los_Angeles: %v", err)
+	}
+	moscow, err := time.LoadLocation("Europe/Moscow")
+	if err != nil {
+		t.Fatalf("failed to load time zone Europe/Moscow: %v", err)
+	}
+
+	t.Log(losAngeles, moscow)
+
+	testCases := []*testCase{
+		{
+			name:      "nullable(decimal)",
+			inputtype: "Nullable(Decimal(9,4))",
+			inputdata: "123",
+			output:    "123",
+		},
+		{
+			name:      "array(nullable(decimal))",
+			inputtype: "Array(Nullable(Decimal(9,4)))",
+			inputdata: "['123','555.6']",
+			output:    []string{"123", "555.6"},
+		},
+		{
+			name:      "nullable(enum)",
+			inputtype: "Nullable(Enum8('hello' = 1, 'world' = 2))",
+			inputdata: "hello",
+			output:    "hello",
+		},
+		{
+			name:      "array(nullable(enum))",
+			inputtype: "Array(Nullable(Enum8('hello' = 1, 'world' = 2)))",
+			inputdata: "['hello','hi']",
+			output:    []string{"hello", "hi"},
+		},
+		{
+			name:      "nullable(uuid)",
+			inputtype: "Nullable(UUID)",
+			inputdata: "c79a9747-7cef-4b11-8177-380f7ed462a4",
+			output:    "c79a9747-7cef-4b11-8177-380f7ed462a4",
+		},
+		{
+			name:      "array(nullable(uuid))",
+			inputtype: "Array(Nullable(UUID))",
+			inputdata: "['c79a9747-7cef-4b11-8177-380f7ed462a4','00000000-0000-0000-0000-000000000000']",
+			output:    []string{"c79a9747-7cef-4b11-8177-380f7ed462a4", "00000000-0000-0000-0000-000000000000"},
+		},
+		{
+			name:      "nullable low cardinality string",
+			inputtype: "Nullable(LowCardinality(String))",
+			inputdata: "hello",
+			output:    "hello",
+		},
+		{
+			name:      "array of nullable low cardinality string",
+			inputtype: "Array(Nullable(LowCardinality(String)))",
+			inputdata: "['hello','hi']",
+			output:    []string{"hello", "hi"},
+		},
+		{
+			name:      "array of nullable low cardinality UInt64",
+			inputtype: "Array(Nullable(LowCardinality(UInt64)))",
+			inputdata: "[123,555]",
+			output:    []uint64{123, 555},
+		},
+		//////////////////////////////////////////////////////
+		{
+			name:      "array(nullable(datetime)), without options and argument",
+			inputtype: "Array(Nullable(DateTime))",
+			inputdata: "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			output: []time.Time{
+				time.Date(2018, 1, 2, 12, 34, 56, 0, time.UTC),
+				{},
+			},
+		},
+		{
+			name:      "array(nullable(datetime)), with argument",
+			inputtype: "Array(Nullable(DateTime('America/Los_Angeles')))",
+			inputdata: "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			output: []time.Time{
+				time.Date(2018, 1, 2, 12, 34, 56, 0, losAngeles),
+				{},
+			},
+		},
+		{
+			name:      "array(nullable(datetime)) with argument, but location nil",
+			inputtype: "Array(Nullable(DateTime('America/Los_Angeles')))",
+			inputdata: "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			inputopt: &DataParserOptions{
+				Location: nil,
+			},
+			output: []time.Time{
+				time.Date(2018, 1, 2, 12, 34, 56, 0, losAngeles),
+				{},
+			},
+		},
+		{
+			name:      "array(nullable(datetime)) without argument, but use location",
+			inputtype: "Array(Nullable(DateTime))",
+			inputdata: "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			inputopt: &DataParserOptions{
+				Location: moscow,
+			},
+			output: []time.Time{
+				time.Date(2018, 1, 2, 12, 34, 56, 0, moscow),
+				{},
+			},
+		},
+		{
+			name:      "array(nullable(datetime)) with argument and location, ingnore argument",
+			inputtype: "Array(Nullable(DateTime('America/Los_Angeles')))",
+			inputdata: "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			inputopt: &DataParserOptions{
+				Location: moscow,
+			},
+			output: []time.Time{
+				time.Date(2018, 1, 2, 12, 34, 56, 0, moscow),
+				{},
+			},
+		},
+		{
+			name:      "array(nullable(datetime)) with argument and location, prefer argument",
+			inputtype: "Array(Nullable(DateTime('America/Los_Angeles')))",
+			inputdata: "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			inputopt: &DataParserOptions{
+				Location:      moscow,
+				UseDBLocation: true,
+			},
+			output: []time.Time{
+				time.Date(2018, 1, 2, 12, 34, 56, 0, losAngeles),
+				{},
+			},
+		},
+		{
+			name:          "array(nullable(datetime)) in nowhere",
+			inputtype:     "Array(Nullable(DateTime('Nowhere')))",
+			inputdata:     "['2018-01-02 12:34:56','0000-00-00 00:00:00']",
+			failNewParser: true,
+		},
+		{
+			name:      "zero array(nullable(datetime))",
+			inputtype: "Array(Nullable(DateTime))",
+			inputdata: "['0000-00-00 00:00:00','0000-00-00 00:00:00']",
+			output: []time.Time{
+				{},
+				{},
+			},
+		},
+		{
+			name:      "short array(nullable(datetime))",
+			inputtype: "Array(Nullable(DateTime))",
+			inputdata: "['000-00-00 00:00:00','000-00-00 00:00:00']",
+			output: []time.Time{
+				{},
+				{},
+			},
+			failParseData: true,
+		},
+		{
+			name:      "malformed array(nullable(datetime))",
+			inputtype: "Array(Nullable(DateTime))",
+			inputdata: "['0000-00-00 00:00:00','a000-00-00 00:00:00']",
+			output: []time.Time{
+				{},
+				{},
+			},
+			failParseData: true,
+		},
+		////////////////////////////////////////////////////
+		{
+			name:      "array of dates",
+			inputtype: "Array(Date)",
+			inputdata: "['2018-01-02','0000-00-00']",
+			output: []time.Time{
+				time.Date(2018, 1, 2, 0, 0, 0, 0, time.UTC),
+				{},
+			},
+		},
+		{
+			name:      "array of nullable dates",
+			inputtype: "Array(Nullable(Date))",
+			inputdata: "['2018-01-02','0000-00-00']",
+			output: []time.Time{
+				time.Date(2018, 1, 2, 0, 0, 0, 0, time.UTC),
+				{},
+			},
+		},
+		{
+			name:          "malformed array(nullable(date))",
+			inputtype:     "Array(Nullable(Date))",
+			inputdata:     "['a000-00-00 00:00:00']",
+			output:        []time.Time{{}},
+			failParseData: true,
+		},
+		{
+			name:      "array of nullable dates with special timezone",
+			inputtype: "Array(Nullable(Date))",
+			inputdata: "['2019-06-29','0000-00-00']",
+			inputopt: &DataParserOptions{
+				Location: losAngeles,
+			},
+			output: []time.Time{
+				time.Date(2019, 6, 29, 0, 0, 0, 0, losAngeles),
+				{},
+			},
+		},
+		//////////////////////////////////////////////////
+		{
+			name:      "array of ints",
+			inputtype: "Array(UInt64)",
+			inputdata: "[1,2,3]",
+			output:    []uint64{1, 2, 3},
+		},
+		{
+			name:      "array of nullable ints",
+			inputtype: "Array(Nullable(UInt64))",
+			inputdata: "[1,2,3]",
+			output:    []uint64{1, 2, 3},
+		},
+		{
+			name:      "empty array of nullable ints",
+			inputtype: "Array(Nullable(UInt64))",
+			inputdata: "[]",
+			output:    []uint64{},
+		},
+		{
+			name:          "bad array of nullable ints",
+			inputtype:     "Array(Nullable(UInt64))",
+			inputdata:     "[1,,]",
+			output:        []uint64{},
+			failParseData: true,
+		},
+		//////////////////////////////////////////////////
+		{
+			name:      "array of strings",
+			inputtype: "Array(String)",
+			inputdata: "['133','2']",
+			output:    []string{"133", "2"},
+		},
+		{
+			name:      "array of nullable strings",
+			inputtype: "Array(Nullable(String))",
+			inputdata: `['a\taa\',','255']`,
+			output:    []string{"a\taa',", "255"},
+		},
+		{
+			name:          "array of nullable strings",
+			inputtype:     "Array(Nullable(String))",
+			inputdata:     "['aaa,]",
+			output:        nil,
+			failParseData: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(tt *testing.T) {
+			desc, err := ParseTypeDesc(tc.inputtype)
+			if tc.failParseDesc {
+				assert.Error(tt, err)
+				return
+			}
+			if !assert.NoError(tt, err) {
+				return
+			}
+			t.Log(desc.Name, desc.Args)
+
+			parser, err := newDataParser(desc, false, tc.inputopt)
+			if tc.failNewParser {
+				assert.Error(tt, err)
+				return
+			}
+			if !assert.NoError(tt, err) {
+				return
+			}
+
+			t.Log(parser.Type(), tc.inputdata)
+			output, err := parser.Parse(strings.NewReader(tc.inputdata))
+			if tc.failParseData {
+				assert.Error(tt, err)
+				return
+			}
+			if !assert.NoError(tt, err) {
+				return
+			}
+
+			assert.Equal(tt, tc.output, output)
+		})
+	}
+}
